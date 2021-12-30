af26b55c9ac650bb0fed396dd0185bf9b18df11e
paakshik <74145408+paakshik@users.noreply.github.com>
2021-12-30 22:21:20 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -229,7 +229,7 @@ textSlot8.innerText = `\nAppCode:${navigator.appCodeName}\nCookie: ${navigator.c
 
 async function modelLoader() {
   return await Promise.all([
-    faceapi.nets.tinyFaceDetector.loadFromUri("/models"), faceapi.nets.faceRecognitionNet.loadFromUri("/models"), faceapi.nets.ssdMobilenetv1.loadFromUri("/models"), faceapi.nets.ageGenderNet.loadFromUri("/models"), faceapi.nets.faceRecognitionNet.loadFromUri("/models"), faceapi.nets.faceLandmark68Net.loadFromUri("/models"), faceapi.nets.faceExpressionNet.loadFromUri("/models")])
+    faceapi.nets.tinyFaceDetector.loadFromUri("/models"), faceapi.nets.ssdMobilenetv1.loadFromUri("/models"), faceapi.nets.ageGenderNet.loadFromUri("/models"), faceapi.nets.faceRecognitionNet.loadFromUri("/models"), faceapi.nets.faceLandmark68Net.loadFromUri("/models"), faceapi.nets.faceExpressionNet.loadFromUri("/models")])
 }
 
  modelLoader().then(async function (e) {
